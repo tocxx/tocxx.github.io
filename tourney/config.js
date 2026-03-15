cd3734b95c57c5f313535dc4f952401194c5c34f
tocxx <realtocxx@gmail.com>
2026-03-15 23:24:50 +0100

    Update config.js

--- tourney/config.js
+++ tourney/config.js
@@ -141,7 +141,7 @@ $(function () {
     );
     if (!tourney) return;
     $.ajax(
-      `https://api.allorigins.win/get?url=` +
+      `http://anyorigin.com/go?url=` +
         encodeURIComponent(
           `https://api.challonge.com/v1/tournaments/${tourney.id}.json?api_key=${APIKey}&include_participants=1&include_matches=1`,
         ),
@@ -288,7 +288,7 @@ $(function () {
       inputAPIKey.removeClass("is-valid");
     } else {
       $.ajax(
-        `https://api.allorigins.win/get?url=` +
+        `http://anyorigin.com/go?url=` +
           encodeURIComponent(
             `https://api.challonge.com/v1/tournaments.json?api_key=${APIKey}`,
           ),
