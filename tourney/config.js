17c6b18fd370e8a343a5ba56895744537494021f
tocxx <realtocxx@gmail.com>
2026-03-15 22:24:41 +0100

    Update config.js

--- tourney/config.js
+++ tourney/config.js
@@ -141,7 +141,7 @@ $(function () {
     );
     if (!tourney) return;
     $.ajax(
-      "https://api.cors.lol/?url=" +
+      "https://crossorigin.me/" +
         encodeURIComponent(
           `https://api.challonge.com/v1/tournaments/${tourney.id}.json?api_key=${APIKey}&include_participants=1&include_matches=1`,
         ),
@@ -288,7 +288,7 @@ $(function () {
       inputAPIKey.removeClass("is-valid");
     } else {
       $.ajax(
-        "https://api.cors.lol/?url=" +
+        "https://crossorigin.me/" +
           encodeURIComponent(
             `https://api.challonge.com/v1/tournaments.json?api_key=${APIKey}`,
           ),
