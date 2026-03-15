b2a81963c72636c7ebd739dcd6b4b78ed81d238a
tocxx <realtocxx@gmail.com>
2026-03-15 22:33:41 +0100

    Update config.js

--- tourney/config.js
+++ tourney/config.js
@@ -141,10 +141,7 @@ $(function () {
     );
     if (!tourney) return;
     $.ajax(
-      "https://crossorigin.me/" +
-        encodeURIComponent(
-          `https://api.challonge.com/v1/tournaments/${tourney.id}.json?api_key=${APIKey}&include_participants=1&include_matches=1`,
-        ),
+      "https://proxy.corsfix.com/?https://api.challonge.com/v1/tournaments/${tourney.id}.json?api_key=${APIKey}&include_participants=1&include_matches=1",
     )
       .done((data) => {
         for (let player of data.tournament.participants) {
@@ -288,10 +285,7 @@ $(function () {
       inputAPIKey.removeClass("is-valid");
     } else {
       $.ajax(
-        "https://crossorigin.me/" +
-          encodeURIComponent(
-            `https://api.challonge.com/v1/tournaments.json?api_key=${APIKey}`,
-          ),
+        "https://proxy.corsfix.com/?https://api.challonge.com/v1/tournaments.json?api_key=${APIKey}",
       )
         .done((data) => {
           inputAPIKey.removeClass("is-invalid");
